e7948cc7a352712d585df62fb91c345b84a1a7b9
Denis (PC) <denistesla2005@gmail.com>
2024-11-29 21:23:53 +0200

    Added local sqlite db support

--- Models/CurrencyMasterDataContext.cs
+++ Models/CurrencyMasterDataContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 
 namespace Currency_Converter;
@@ -8,18 +9,27 @@ public partial class CurrencyMasterDataContext : DbContext
 {
     public CurrencyMasterDataContext()
     {
+        Database.EnsureCreated();
     }
 
     public CurrencyMasterDataContext(DbContextOptions<CurrencyMasterDataContext> options)
         : base(options)
     {
+        Database.EnsureCreated();
     }
 
     public virtual DbSet<CurrencyMaster> CurrencyMasters { get; set; }
 
+    #if DEBUG
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Computer-Science\\WPF\\Currency Converter\\Database\\Currency Converter.mdf;Integrated Security=True");
+    #else
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        string path = Environment.ExpandEnvironmentVariables(@"%appdata%\DarkDB\CurrencyConverter.db");
+        Directory.CreateDirectory(Path.GetDirectoryName(path)!);
+        optionsBuilder.UseSqlite("Data Source="+path);
+    }
+    #endif
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
